e0da576a3e6d58a7d324577922b014c3e746e829
nikikust <52621231+nikikust@users.noreply.github.com>
2020-03-22 12:50:04 +0300

    Update shuffle.py

--- modules/shuffle.py
+++ modules/shuffle.py
@@ -5,7 +5,7 @@ from math import sqrt
 
 def gen_shuffle(data):
     num = len(data)
-    razbros, itog = int(sqrt(num)), [-1 for i in range(num)]
+    spread, result = int(sqrt(num)), [-1 for i in range(num)]
     for i in range(num):
-        itog[(i + i * razbros) % num] = data[i]
-    return itog
+        result[(i + i * spread) % num] = data[i]
+    return result
